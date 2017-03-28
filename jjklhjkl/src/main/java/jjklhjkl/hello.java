5301d049a353a6f9ac144855764cd74ade32a4ca
cs <csayouri@hotmail.fr>
2017-03-28 18:30:21 +0200

    hel

--- jjklhjkl/src/main/java/jjklhjkl/hello.java
+++ jjklhjkl/src/main/java/jjklhjkl/hello.java
@@ -1,7 +1,16 @@
 package jjklhjkl;
 
 public class hello {
+	public int bidon(){
+		return 3;
+	}
+	
+	public  int division(int a, int b){
+		int x =bidon();
+		return a/b;
+	}
 	public static void main(String[] args) {
+		new hello().division(3,0);
 		
 	}
 
